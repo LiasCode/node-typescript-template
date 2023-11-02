87a572f86c5280e1c86b9634a4744f7a7ec199bd
LiasCode <liasraul00@gmail.com>
2023-11-02 15:56:27 -0400

    update dependencies and change to pnpm

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import bodyParser from "body-parser";
+import compression from "compression";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// --------- SETUP APP -------------
+export const App = express();
+
+// ---------- APP SETTINGS ------------
+App.set("env", "production");
+App.set("PORT", process.env.PORT || 3030);
+App.disable("x-powered-by");
+
+// ---------- MIDDLEWARES ---------
+App.use(compression());
+App.use(helmet());
+App.use(morgan("dev"));
+App.use(cors());
+App.use(bodyParser.urlencoded({ extended: false }));
+App.use(bodyParser.json());
+App.use(bodyParser.text());
+App.use(bodyParser.raw());
+
+// ------------- ROUTER ------------
+App.use(express.static(process.cwd() + "/public"));
+
+App.get("*", (_, res) => {
+  return res.send("<h1>Hello World</h1>");
+});
